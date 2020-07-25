b1c40eddc046ead61a4cf15c7137e4fdf23d24c9
Chethan Bhat <chethanbhat@gmail.com>
2020-07-25 16:05:27 +0530

    Deploy to github

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Image Gallery
+
+React Image Gallery is a image search app built using React and Tailwind. It searches for photos on Pixabay using Pixabay official API.
+
+Some of the cool features of this project
+
+- Tailwind for Styling
+- User can search for images using keywords
+
+## Demo
+
+[Live App](https://thunder-photon-flash.netlify.app/) deployed on netlify.
+
+## Author
+
+[Chethan Bhat](https://chethanbhat.com)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto p-4 md:p-4 lg:p-0">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mt-32">No Images found !</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mt-32">Image Loading</h1>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
